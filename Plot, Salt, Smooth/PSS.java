9c321270dfbc93a27d38685face0606b4fd1b0b3
Kyle <krgamer444@gmail.com>
2023-12-10 15:06:52 -0500

    finish pss

--- Plot, Salt, Smooth/PSS.java
+++ Plot, Salt, Smooth/PSS.java
@@ -26,6 +26,13 @@ public class PSS {
         return data;
     }
 
+    /**
+     * Salts data by adding a random number generated from given lower and upper bounds.
+     * @param data - Data to salt
+     * @param lower - Lower bound
+     * @param upper - Upper bound
+     * @return - An ArrayList of Salted data points (Y - values)
+     */
     public static ArrayList<Integer> saltData(ArrayList<Integer> data, int lower, int upper){
         ArrayList<Integer> salted = new ArrayList<Integer>();
         for(int i = 0; i < data.size(); i++){
@@ -35,10 +42,49 @@ public class PSS {
         return salted;
     }
 
+    /**
+     * Smooths data in a given set (of integers).
+     * @param data - ArrayList of integers to smooth.
+     * @param range - +- range for any given point. Ex: Taking the left most 2 points and right most 2 points.
+     * @return - An ArrayList of new, smoothed data values (Y - values)
+     */
     public static ArrayList<Integer> smoothData(ArrayList<Integer> data, int range){
         ArrayList<Integer> smoothed = new ArrayList<Integer>();
-        
-
+        for(int i = 0; i < data.size(); i++){
+            int point = data.get(i);
+            int lower_total = 0;
+            int upper_total = 0;
+            int points = 1;
+            // Traverse lower and upper, without going out of bounds.
+            if(i!= 0 && i != data.size() - 1){
+                for(int j = 1; j <= range; j++){
+                    if(data.get(i - j) != null){
+                        points++;
+                        lower_total = lower_total + data.get(i - j);
+                    }
+                    if(data.get(i + j) != null){
+                        points++;
+                        upper_total = upper_total + data.get(i + j);
+                    }
+                }
+            } else if(i == 0){
+                for(int j = 1; j <= range; j++){
+                    if(data.get(i + j) != null){
+                        points++;
+                        upper_total = upper_total + data.get(i + j);
+                    }
+                }
+            } else if(i == data.size() - 1){
+                for(int j = 1; j <= range; j++){
+                    if(data.get(i - j) != null){
+                        points++;
+                        lower_total = lower_total + data.get(i - j);
+                    }
+                }
+            }
+            int newVal = upper_total + lower_total + point / points;
+            smoothed.add(newVal);
+        }
         return smoothed;
     }
 
